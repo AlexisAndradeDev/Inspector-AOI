51f1de24bc4d1d7117d76e654116b88be4632aa1
Alexis Martínez <alexis.martinez@dexill.com>
2020-01-07 04:21:37 +0000

    + Cambiados caracteres UTF8 a ANSI

--- plibs/inspector_package/operations.py
+++ plibs/inspector_package/operations.py
@@ -3,8 +3,8 @@ from cv2 import imwrite
 class ObjectInspected:
     def __init__(self,board_number):
         self.number = board_number
-        # el Ã­ndice es igual al nÃºmero del tablero menos uno, ya que el Ã­ndice
-        # es usado para posiciones en lista, cuya primera posiciÃ³n es 0.
+        # el í­ndice es igual al número del tablero menos uno, ya que el índice
+        # es usado para posiciones en lista, cuya primera posición es 0.
         self.index = board_number-1
         self.status = "good" # iniciar como "bueno" por defecto
         self.inspection_points_results = ""
@@ -30,9 +30,9 @@ class ObjectInspected:
         self.results += inspection_point_results
     def set_board_results(self, registration_time, inspection_time, stage):
         """
-        El parÃ¡metro << stage >> se refiere a la etapa en la que se estÃ¡: debugeo o inspecciÃ³n.
-        Si se estÃ¡ en debugeo, el tiempo de registro no se escribe ya que no se registra el tablero en debugeo.
-        Si se estÃ¡ en inspecciÃ³n, se escribe el tiempo de registro e inspecciÃ³n.
+        El parámetro << stage >> se refiere a la etapa en la que se está: debugeo o inspección.
+        Si se está en debugeo, el tiempo de registro no se escribe ya que no se registra el tablero en debugeo.
+        Si se está en inspección, se escribe el tiempo de registro e inspección.
         """
         if stage == "inspection":
             self.board_results = "&{0};{1};{2};{3}#".format(
@@ -54,40 +54,40 @@ class ObjectInspected:
         return self.status
     def get_inspection_points_results(self):
         """
-        Resultados de cada punto de inspecciÃ³n:
-            * NÃºmero de tablero
+        Resultados de cada punto de inspección:
+            * Número de tablero
             * Nombre del punto
             * Status del punto (good, bad, failed)
-            * Resultados de la funciÃ³n de inspecciÃ³n (Ã¡rea de blob, calificaciÃ³n de tm)
-            * Fallos del punto (si no hubo, es una lista vacÃ­a [] )
+            * Resultados de la función de inspección (área de blob, calificación de tm)
+            * Fallos del punto (si no hubo, es una lista vací­a [] )
         """
         return self.inspection_points_results
     def get_board_results(self):
         """
         Resultados del tablero:
-            * NÃºmero de tablero
+            * Número de tablero
             * Status del tablero (good, bad, failed, skip, registration_failed, error)
-            * (SI SE ESTÃ EN LA ETAPA DE INSPECCIÃN): Tiempo de registro
-            * Tiempo de inspecciÃ³n
+            * (SI SE ESTÁ EN LA ETAPA DE INSPECCIÓN): Tiempo de registro
+            * Tiempo de inspección
         """
         return self.board_results
     def get_results(self):
         """
-        Resultados de los puntos de inspecciÃ³n y del tablero combinados.
+        Resultados de los puntos de inspección y del tablero combinados.
         """
         return self.results
 
 
 def export_images(images, board_number, ins_point_name, light, images_path):
-    # Exportar imÃ¡genes del proceso de la funciÃ³n skip
+    # Exportar imágenes del proceso de la función skip
     for image_name, image in images:
-        # num_de_tablero-nombre_de_punto_de_inspecciÃ³n-luz_usada(ultraviolet/white)-nombre_de_imagen
+        # num_de_tablero-nombre_de_punto_de_inspección-luz_usada(ultraviolet/white)-nombre_de_imagen
         imwrite("{0}{1}-{2}-{3}-{4}.bmp".format(images_path, board_number, ins_point_name, light, image_name), image)
 
 def add_to_images_name(images, str_):
     """
     Es utilizado para agregar una cadena de texto al nombre de todas las
-    imÃ¡genes que son retornadas por funciones de inspecciÃ³n y mÃ©todos de registro
+    imágenes que son retornadas por funciones de inspección y métodos de registro
     para ser exportadas.
     """
     for image_index in range(len(images)):
