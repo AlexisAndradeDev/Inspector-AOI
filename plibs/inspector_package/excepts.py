6f8ecea3cf7464a4845f94814e31f12f952b8f2b
Alexis Martínez <alexis.martinez@dexill.com>
2020-01-11 00:02:10 +0000

    + Corregidos caracteres UTF8

--- plibs/inspector_package/excepts.py
+++ plibs/inspector_package/excepts.py
@@ -1,13 +1,13 @@
 class MT_ERROR(Exception):
     """Se lanza al tener un error al utilizar cv2.matchTemplate en find_matches
-    del mÃ³dulo cv_func."""
+    del módulo cv_func."""
     def __init__(self):
-        # al convertir a str la excepciÃ³n, su valor serÃ¡ args.
+        # al convertir a str la excepción, su valor será args.
         self.args = ["MT_ERROR"]
 
 class UNKNOWN_CF_ERROR(Exception):
-    """Se lanza al tener un error en el bucle para filtrar mÃºltiples coincidencias
-    en find_matches del mÃ³dulo cv_func."""
+    """Se lanza al tener un error en el bucle para filtrar múltiples coincidencias
+    en find_matches del módulo cv_func."""
     def __init__(self):
-        # al convertir a str la excepciÃ³n, su valor serÃ¡ args.
+        # al convertir a str la excepción, su valor será args.
         self.args = ["UNKNOWN_CF_ERROR"]
